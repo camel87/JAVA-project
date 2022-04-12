5ed0ddbd5d213d0136f5e143766e4cdfc78cc5c2
리미 <camel87@naver.com>
2022-04-12 00:05:58 +0000

    1번 파일 수정

---
+++ test/HelloEx001.java
@@ -0,0 +1,10 @@
+public class HelloEx001 {
+    public static void main(String[] args) {
+        int x=100;
+        int y=200;
+        System.out.println( "x+y="+(x+y) );
+        System.out.println( "x-y="+(x-y) );
+        System.out.println( "x*y="+(x*y) );
+        System.out.println( "x/y="+(x/y) );
+    }
+}
